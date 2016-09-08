6aff226e29391cb92a4fe791b7014e821cfa1a37
taneotsa <taneotsa@greeny.cs.tlu.ee>
2016-09-08 15:16:28 +0300

    added my first lines of code

---
+++ first.php
@@ -0,0 +1,14 @@
+﻿<?php
+	$first_name="Tanel";
+	$last_name="Otsa";
+	
+	// käsk echo trükib muutuja väärtuse
+	
+	//echo $first_name;
+	//echo $last_name;
+	//2 väärtuse liitmine käib punktiga
+	
+	echo $first_name." ".$last_name;
+	
+	
+?>
